b710e6ab1ca90cabdb6d5a836eadd357abe7b82c
Immanuel Fernandes <152952734+Immanuel-Fernandes@users.noreply.github.com>
2025-07-24 20:55:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,8 +36,19 @@ document.addEventListener('DOMContentLoaded', () => {
         animationFrameId: null,
     };
 
-    const charMap = {
-        '!': '!.png', '&': '&.png', ',': ',.png', '-': '-.png', '0': '0.png', '1': '1.png', '2': '2.png', '3': '3.png', '4': '4.png', '5': '5.png', '6': '6.png', '7': '7.png', '8': '8.png', '9': '9.png', 'A': 'A.png', 'B': 'B.png', 'C': 'C.png', 'D': 'D.png', 'E': 'E.png', 'F': 'F.png', 'G': 'G.png', 'H': 'H.png', 'I': 'I.png', 'J': 'J.png', 'K': 'K.png', 'L': 'L.png', 'M': 'M.png', 'N': 'N.png', 'O': 'O.png', 'P': 'P.png', 'Q': 'Q.png', 'R': 'R.png', 'S': 'S.png', 'T': 'T.png', 'U': 'U.png', 'V': 'V.png', 'W': 'W.png', 'X': 'X.png', 'Y': 'Y.png', 'Z': 'Z.png', '?': 'QM.png', '/': 'SL.png', ' ': 'SP.png'
+   const charMap = {
+        '!': '!.png', '&': '&.png', ',': ',.png', '-': '-.png', 
+        '0': '0.png', '1': '1.png', '2': '2.png', '3': '3.png', 
+        '4': '4.png', '5': '5.png', '6': '6.png', '7': '7.png', 
+        '8': '8.png', '9': '9.png', '.': 'Dot.png', '+': '+.png',
+        'A': 'A.png', 'B': 'B.png', 'C': 'C.png', 'D': 'D.png', 
+        'E': 'E.png', 'F': 'F.png', 'G': 'G.png', 'H': 'H.png', 
+        'I': 'I.png', 'J': 'J.png', 'K': 'K.png', 'L': 'L.png', 
+        'M': 'M.png', 'N': 'N.png', 'O': 'O.png', 'P': 'P.png', 
+        'Q': 'Q.png', 'R': 'R.png', 'S': 'S.png', 'T': 'T.png', 
+        'U': 'U.png', 'V': 'V.png', 'W': 'W.png', 'X': 'X.png', 
+        'Y': 'Y.png', 'Z': 'Z.png', 
+        '?': 'QM.png', '/': 'SL.png', ' ': 'SP.png'
     };
     
     // --- Animation Logic ---
